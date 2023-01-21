bc0c81bbd25a133f9791e32cf28d357d606d8485
vansari <junker.kurt@googlemail.com>
2023-01-21 15:01:46 +0100

    fixed ID

--- src/Entity/AbstractBasic.php
+++ src/Entity/AbstractBasic.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\DiscriminatorColumn;
 use Doctrine\ORM\Mapping\DiscriminatorMap;
 use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\InheritanceType;
 use Doctrine\ORM\Mapping\Table;
@@ -24,6 +25,7 @@ use Doctrine\ORM\Mapping\Table;
 class AbstractBasic
 {
     #[Id, Column(type: 'integer')]
+    #[GeneratedValue(strategy: 'IDENTITY')]
     protected ?int $id = null;
 
     #[Column(type: 'string')]
